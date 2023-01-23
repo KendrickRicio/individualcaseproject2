e19e2e634b662b605ce4278d7a7b354a1cf76707
unknown <kendrickricio@gmail.com>
2023-01-23 00:59:21 -0800

    fix for github pages

--- js/calculator.js
+++ js/calculator.js
@@ -14,32 +14,20 @@ function updateTotal() {
 }
 
 function loadData() {
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', '../data/ships.json', true);
-    xhr.responseType = 'json';
-    xhr.onload = function() {
-        if (xhr.status === 200) {
-            shipData = xhr.response;
-            let form = document.getElementById("shipForm");
-            for (var ship in shipData) {
-                let checkbox = document.createElement("input");
-                checkbox.type = "checkbox";
-                checkbox.name = "ship";
-                checkbox.value = shipData[ship].price;
-                form.appendChild(checkbox);
-                form.innerHTML += ship + " (" + shipData[ship].price + " AUEC)<br>";
-                form.addEventListener("click", function(event) {
-                    if (event.target.type === "checkbox") {
-                        updateTotal();
-                    }
-                });
-            };
-        } else {
-            console.error(xhr.statusText);
-        }
+    console.log(shipCosts);
+    shipData = JSON.parse(shipCosts);
+    let form = document.getElementById("shipForm");
+    for (var ship in shipData) {
+        let checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+        checkbox.name = "ship";
+        checkbox.value = shipData[ship].price;
+        form.appendChild(checkbox);
+        form.innerHTML += ship + " (" + shipData[ship].price + " AUEC)<br>";
+        form.addEventListener("click", function(event) {
+            if (event.target.type === "checkbox") {
+                updateTotal();
+            }
+        });
     };
-    xhr.onerror = function(e) {
-        console.error(xhr.statusText);
-    };
-    xhr.send();
 }
